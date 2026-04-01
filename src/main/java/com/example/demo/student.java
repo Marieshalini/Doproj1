ed361d1ffd5e7bffa344e887ad131d34f87207b6
23CS147 <23CS147@licet.com>
2026-04-01 09:15:19 +0530

    initial commit

--- src/main/java/com/example/demo/student.java
+++ src/main/java/com/example/demo/student.java
@@ -6,13 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class student {
 
-    @GetMapping("/")
-    public String home() {
-        return "index"; 
-    }
-
     @GetMapping("/greet")
-    @ResponseBody
     public String greetUser(@RequestParam String name) {
         return "Hello " + name;
     }
